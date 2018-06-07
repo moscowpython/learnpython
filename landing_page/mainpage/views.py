e8dd2fd0d749a48c1170b8a80a105a38874368e4
Simon Osipov <osipov.simon@gmail.com>
2018-06-07 23:45:11 +0300

    Some update

--- landing_page/mainpage/views.py
+++ landing_page/mainpage/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-from .models import MoscowPythonMeetup
+from .models import MoscowPythonMeetup, LearnPythonCourse
 
 
 def index(request):
 	template = loader.get_template('mainpage/index.html')
 	current_meetup = MoscowPythonMeetup.objects.latest('meetup_number')
+	current_course = LearnPythonCourse.objects.latest('course_index')
 	context = {
-		'meetup': current_meetup
+		'meetup': current_meetup,
+		'course': current_course
 	}
 	return HttpResponse(template.render(context, request))
 
