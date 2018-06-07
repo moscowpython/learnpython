7999073acaa839c716be1ebe97ab2f07da348238
Simon Osipov <osipov.simon@gmail.com>
2018-06-07 11:39:16 +0300

    Initiate view

--- landing_page/mainpage/views.py
+++ landing_page/mainpage/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 
 def index(request):
-	return HttpResponse("Hello, world. You're at the polls index.")
-
+	template = loader.get_template('mainpage/index.html')
+	context = {'number': 5}
+	return HttpResponse(template.render(context, request))
 
