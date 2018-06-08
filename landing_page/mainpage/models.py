01c068bb4ba4e1c8779dfe68ae9f22e4f3e37d70
Simon Osipov <osipov.simon@gmail.com>
2018-06-08 15:28:55 +0300

    Save some progress

--- landing_page/mainpage/models.py
+++ landing_page/mainpage/models.py
@@ -2,268 +2,267 @@ from django.db import models
 
 
 class MoscowPythonMeetup(models.Model):
+    class Meta:
+        verbose_name_plural = "MoscowPython Митапы"
 
-	class Meta:
-		verbose_name_plural = "MoscowPython Митапы"
+    def __str__(self):
+        return f'MoscowPython Meetup № {self.meetup_number}'
 
-	def __str__(self):
-		return f'MoscowPython Meetup № {self.meetup_number}'
+    meetup_number = models.IntegerField(
+        verbose_name='Номер митапа',
+        help_text='Введите номер митапа'
+    )
 
-	meetup_number = models.IntegerField(
-		verbose_name='Номер митапа',
-		help_text='Введите номер митапа'
-	)
+    meetup_day = models.IntegerField(
+        verbose_name='День Митапа',
+        help_text='День месяца, в который будет митап'
+    )
 
-	meetup_day = models.IntegerField(
-		verbose_name='День Митапа',
-		help_text='День месяца, в который будет митап'
-	)
+    months_list = [
+        ('Января', 'Январь'),
+        ('Февраля', 'Февраль'),
+        ('Марта', 'Март'),
+        ('Апреля', 'Апрель'),
+        ('Мая', 'Май'),
+        ('Июня', 'Июнь'),
+        ('Июля', 'Июль'),
+        ('Августа', 'Август'),
+        ('Сентября', 'Сентябрь'),
+        ('Октября', 'Октябрь'),
+        ('Ноября', 'Ноябрь'),
+        ('Декабря', 'Декабрь')
+    ]
 
-	months_list = [
-		('Января', 'Январь'),
-		('Февраля', 'Февраль'),
-		('Марта', 'Март'),
-		('Апреля', 'Апрель'),
-		('Мая', 'Май'),
-		('Июня', 'Июнь'),
-		('Июля', 'Июль'),
-		('Августа', 'Август'),
-		('Сентября', 'Сентябрь'),
-		('Октября', 'Октябрь'),
-		('Ноября', 'Ноябрь'),
-		('Декабря', 'Декабрь')
-	]
+    meetup_month = models.TextField(
+        choices=months_list,
+        default='Январь',
+        verbose_name='Месяц Митапа',
+        help_text='Месяц, в котором будет проходить митап'
+    )
 
-	meetup_month = models.TextField(
-		choices=months_list,
-		default='Январь',
-		verbose_name='Месяц Митапа',
-		help_text='Месяц, в котором будет проходить митап'
-	)
+    meetup_time = models.TimeField(
+        blank=True,
+        auto_now=True,
+        verbose_name='Время Митапа',
+        help_text='Время начала митапа'
+    )
 
-	meetup_time = models.TimeField(
-		blank=True,
-		auto_now=True,
-		verbose_name='Время Митапа',
-		help_text='Время начала митапа'
-	)
-
-	meetup_link = models.URLField(
-		verbose_name='Ссылка на митап',
-		default='http://www.moscowpython.ru',
-		help_text='Ссылка по стандарту, можно менять'
-	)
+    meetup_link = models.URLField(
+        verbose_name='Ссылка на митап',
+        default='http://www.moscowpython.ru',
+        help_text='Ссылка по стандарту, можно менять'
+    )
 
 
 class LearnPythonCourse(models.Model):
-	class Meta:
-		verbose_name_plural = "LearnPython Наборы"
+    class Meta:
+        verbose_name_plural = "LearnPython Наборы"
 
-	def __str__(self):
-		return f'LearnPython Набор № {self.course_index}'
+    def __str__(self):
+        return f'LearnPython Набор № {self.course_index}'
 
-	course_index = models.IntegerField(
-		verbose_name='Набор №',
-		help_text='Порядковый номер набора'
-	)
+    course_index = models.IntegerField(
+        verbose_name='Набор №',
+        help_text='Порядковый номер набора'
+    )
 
-	course_start_date = models.DateField(
-		verbose_name='Дата начала курса',
-		help_text='Дата первого занятия'
-	)
+    course_start_date = models.DateField(
+        verbose_name='Дата начала курса',
+        help_text='Дата первого занятия'
+    )
 
-	course_end_date = models.DateField(
-		verbose_name='Дата окончания курса',
-		help_text='Дата последнего занятия',
-	)
+    course_end_date = models.DateField(
+        verbose_name='Дата окончания курса',
+        help_text='Дата последнего занятия',
+    )
 
-	end_registration_date = models.DateTimeField(
-		verbose_name='Дата закрытия регистрации',
-		help_text='Во сколько закрывается регистрация?',
-		default=None,
+    end_registration_date = models.DateTimeField(
+        verbose_name='Дата закрытия регистрации',
+        help_text='Во сколько закрывается регистрация?',
+        default=None,
 
-	)
+    )
 
 
 class LearnPythonCoursePrices(models.Model):
-	class Meta:
-		verbose_name_plural = 'LearnPython Цены на курсы'
+    class Meta:
+        verbose_name_plural = 'LearnPython Цены на курсы'
 
-	def __str__(self):
-		return f'Интервал {self.price_range} на {self.course_type}'
+    def __str__(self):
+        return f'Интервал {self.price_range} на {self.course_type}'
 
-	price_range = models.IntegerField(
-		verbose_name='Название временного отрезка',
-		help_text='Мустанг / Гепард / Панда и вот это все',
-		default=None
-	)
+    price_range = models.IntegerField(
+        verbose_name='Название временного отрезка',
+        help_text='Мустанг / Гепард / Панда и вот это все',
+        default=None
+    )
 
-	course_type = [
-		('Online', 'Online'),
-		('Offline', 'Offline')
-	]
+    course_type = [
+        ('Online', 'Online'),
+        ('Offline', 'Offline')
+    ]
 
-	course_type = models.TextField(
-		choices=course_type,
-		verbose_name='Тип курса',
-		help_text='Выберите тип курса'
-	)
+    course_type = models.TextField(
+        choices=course_type,
+        verbose_name='Тип курса',
+        help_text='Выберите тип курса'
+    )
 
-	price_range_end_date = models.DateField(
-		verbose_name='Дата окончания интервала',
-		help_text='Когда истекает данное предложение'
-	)
+    price_range_end_date = models.DateField(
+        verbose_name='Дата окончания интервала',
+        help_text='Когда истекает данное предложение'
+    )
 
-	price_range_price = models.IntegerField(
-		verbose_name='Цена',
-		help_text='Сколько стоит курс в этот период'
-	)
+    price_range_price = models.IntegerField(
+        verbose_name='Цена',
+        help_text='Сколько стоит курс в этот период'
+    )
 
 
 class Courators(models.Model):
-	class Meta:
-		verbose_name_plural = 'LearnPython Кураторы'
+    class Meta:
+        verbose_name_plural = 'LearnPython Кураторы'
 
-	def __str__(self):
-		return f'Куратор {self.curator_name}'
+    def __str__(self):
+        return f'Куратор {self.curator_name}'
 
-	curator_name = models.CharField(
-		max_length=50,
-		verbose_name='Имя и фамилия куратора',
-		help_text='Сначало имя, потом фамилия'
-	)
+    curator_name = models.CharField(
+        max_length=50,
+        verbose_name='Имя и фамилия куратора',
+        help_text='Сначало имя, потом фамилия'
+    )
 
-	curator_bio = models.TextField(
-		verbose_name='Биография куратора',
-		help_text='Где и кем работает, что программирует'
-	)
+    curator_bio = models.TextField(
+        verbose_name='Биография куратора',
+        help_text='Где и кем работает, что программирует'
+    )
 
-	curator_motto = models.CharField(
-		max_length=150,
-		verbose_name='Девиз куратора',
-		help_text='Через тернии к звездам или что-нибудь такое'
-	)
+    curator_motto = models.CharField(
+        max_length=150,
+        verbose_name='Девиз куратора',
+        help_text='Через тернии к звездам или что-нибудь такое'
+    )
 
-	curator_photo = models.ImageField(
-		help_text='Фотография куратора',
-		verbose_name='Фото куратора'
-	)
+    curator_photo = models.ImageField(
+        help_text='Фотография куратора',
+        verbose_name='Фото куратора'
+    )
 
-	courator_status = models.BooleanField(
-		verbose_name="Куратор для ближайшего набора?",
-		help_text='Куратор работает в текущем наборе?',
-		default=True
-	)
+    courator_status = models.BooleanField(
+        verbose_name="Куратор для ближайшего набора?",
+        help_text='Куратор работает в текущем наборе?',
+        default=True
+    )
 
-	courator_github = models.URLField(
-		blank=True,
-		help_text='Ссылка на гит-хаб куратора',
-		verbose_name='GitHub куратора'
-	)
+    courator_github = models.URLField(
+        blank=True,
+        help_text='Ссылка на гит-хаб куратора',
+        verbose_name='GitHub куратора'
+    )
 
-	courator_social_network = models.URLField(
-		blank=True,
-		help_text='Если есть, ссылка на соцсеточку',
-		verbose_name='Соцсеточка куратора'
-	)
+    courator_social_network = models.URLField(
+        blank=True,
+        help_text='Если есть, ссылка на соцсеточку',
+        verbose_name='Соцсеточка куратора'
+    )
 
 
 class Feedback(models.Model):
-	class Meta:
-		verbose_name_plural = "LearnPython Отзывы"
+    class Meta:
+        verbose_name_plural = "LearnPython Отзывы"
 
-	def __str__(self):
-		return f'Отзыв участника {self.feedback_author}'
+    def __str__(self):
+        return f'Отзыв участника {self.feedback_author}'
 
-	feedback_author = models.CharField(
-		verbose_name='Автор отзыва',
-		help_text='Кто автор отзыва?',
-		max_length=50,
-		null=True
-	)
+    feedback_author = models.CharField(
+        verbose_name='Автор отзыва',
+        help_text='Кто автор отзыва?',
+        max_length=50,
+        null=True
+    )
 
-	feedback_author_link = models.URLField(
-		verbose_name='Ссылка на автора',
-		help_text='Есть ли ссылка на соцсеточки автора?',
-		blank=True,
-		null=True
-	)
+    feedback_author_link = models.URLField(
+        verbose_name='Ссылка на автора',
+        help_text='Есть ли ссылка на соцсеточки автора?',
+        blank=True,
+        null=True
+    )
 
-	feedback_author_position = models.CharField(
-		verbose_name='Должность выпускника',
-		help_text='Кем работает автор?',
-		max_length=50,
-		null=True
-	)
+    feedback_author_position = models.CharField(
+        verbose_name='Должность выпускника',
+        help_text='Кем работает автор?',
+        max_length=50,
+        null=True
+    )
 
-	feedback_author_photo = models.ImageField(
-		verbose_name='Фотография автора',
-		help_text='Прикрепите фото автора отзыва',
-		null=True
-	)
+    feedback_author_photo = models.ImageField(
+        verbose_name='Фотография автора',
+        help_text='Прикрепите фото автора отзыва',
+        null=True
+    )
 
-	feedback_text = models.TextField(
-		verbose_name='Текст отзыва',
-		help_text='А сюда пишем отзыв автора',
-		null=True
-	)
+    feedback_text = models.TextField(
+        verbose_name='Текст отзыва',
+        help_text='А сюда пишем отзыв автора',
+        null=True
+    )
 
 
 class GraduateStories(models.Model):
-	class Meta:
-		verbose_name_plural = 'Learn Python Истории учеников'
+    class Meta:
+        verbose_name_plural = 'Learn Python Истории учеников'
 
-	def __str__(self):
-		return f'История участника {self.story_author}'
+    def __str__(self):
+        return f'История участника {self.story_author}'
 
-	story_author = models.CharField(
-		verbose_name='Автор истории',
-		help_text='Как зовут автора истории?',
-		max_length=50,
-		null=True
-	)
+    story_author = models.CharField(
+        verbose_name='Автор истории',
+        help_text='Как зовут автора истории?',
+        max_length=50,
+        null=True
+    )
 
-	story_author_photo = models.ImageField(
-		verbose_name='Фотография автора',
-		help_text='Прикрепите фото автора истории',
-		null=True
-	)
+    story_author_photo = models.ImageField(
+        verbose_name='Фотография автора',
+        help_text='Прикрепите фото автора истории',
+        null=True
+    )
 
-	story_author_position = models.CharField(
-		verbose_name='Должность выпускника',
-		help_text='Кто теперь выпускник',
-		max_length=50,
-		null=True
-	)
+    story_author_position = models.CharField(
+        verbose_name='Должность выпускника',
+        help_text='Кто теперь выпускник',
+        max_length=50,
+        null=True
+    )
 
-	story_author_background = models.CharField(
-		verbose_name='В прошлом автор был:',
-		help_text='Прим. "В прошлом: терапевт"',
-		max_length=150
-	)
+    story_author_background = models.CharField(
+        verbose_name='В прошлом автор был:',
+        help_text='Прим. "В прошлом: терапевт"',
+        max_length=150
+    )
 
-	story_text = models.TextField(
-		verbose_name='История пользователя',
-		help_text='Что хотел бы автор рассказать',
-		default=None
-	)
+    story_text = models.TextField(
+        verbose_name='История пользователя',
+        help_text='Что хотел бы автор рассказать',
+        default=None
+    )
 
 
 class GraduateProjects(models.Model):
-	class Meta:
-		verbose_name_plural = 'LearnPython Проекты Учеников'
+    class Meta:
+        verbose_name_plural = 'LearnPython Проекты Учеников'
 
-	def __str__(self):
-		return f'Проект "{self.project_name}"'
+    def __str__(self):
+        return f'Проект "{self.project_name}"'
 
-	project_name = models.CharField(
-		max_length=150,
-		help_text='Название проекта',
-		default=None
-	)
+    project_name = models.CharField(
+        max_length=150,
+        help_text='Название проекта',
+        default=None
+    )
 
-	project_image = models.ImageField(
-		help_text="Пример интерфейса проекта",
-		default=None,
-		upload_to='projects/'
-	)
+    project_image = models.ImageField(
+        help_text="Пример интерфейса проекта",
+        default=None,
+        upload_to='projects/'
+    )
--- landing_page/mainpage/views.py
+++ landing_page/mainpage/views.py
@@ -1,19 +1,46 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-from .models import MoscowPythonMeetup, LearnPythonCourse, GraduateProjects
+from .models import MoscowPythonMeetup, LearnPythonCourse, GraduateProjects, LearnPythonCoursePrices
 
 
 def index(request):
-	template = loader.get_template('mainpage/index.html')
-	current_meetup = MoscowPythonMeetup.objects.latest('meetup_number')
-	current_course = LearnPythonCourse.objects.latest('course_index')
-	student_projects = list(GraduateProjects.objects.all())
+    template = loader.get_template('mainpage/index.html')
 
-	context = {
-		'meetup': current_meetup,
-		'course': current_course,
-		'projects': student_projects
-	}
-	return HttpResponse(template.render(context, request))
+    months_list = (
+        'Января',
+        'Февраля',
+        'Марта',
+        'Апреля',
+        'Мая',
+        'Июня',
+        'Июля',
+        'Августа',
+        'Сентября',
+        'Октября',
+        'Ноября',
+        'Декабря'
+    )
 
+    current_course = LearnPythonCourse.objects.latest('course_index')
+    course_month_start = LearnPythonCourse.objects.all()[:1].get().course_start_date
+    course_month_end = LearnPythonCourse.objects.all()[:1].get().course_end_date
+    lowest_online_price = LearnPythonCoursePrices.objects.filter(course_type='Online').order_by('price_range_price')
+    lowest_offline_price = LearnPythonCoursePrices.objects.filter(course_type='Offline').order_by('price_range_price')
+
+    student_projects = list(GraduateProjects.objects.all())
+
+    current_meetup = MoscowPythonMeetup.objects.latest('meetup_number')
+
+    context = {
+        'meetup': current_meetup,
+        'course': current_course,
+        'projects': student_projects,
+        'lowest_online_course_price': lowest_online_price[0],
+        'lowest_offline_course_price': lowest_offline_price[0],
+        'course_day_start': course_month_start.day,
+        'course_day_end': course_month_end.day,
+        'course_month_start': months_list[course_month_start.month - 1],
+        'course_month_end': months_list[course_month_end.month - 1]
+    }
+    return HttpResponse(template.render(context, request))
