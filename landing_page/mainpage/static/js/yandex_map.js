9d0fe24648624135c3f630c92cad237f6079001f
Simon Osipov <osipov.simon@gmail.com>
2018-08-04 05:51:10 +0300

    Changed icon of point

--- landing_page/mainpage/static/js/yandex_map.js
+++ landing_page/mainpage/static/js/yandex_map.js
@@ -1,15 +1,25 @@
-var myMap;
-var myPlacemark;
-
 ymaps.ready(init);
 
 function init () {
-    myMap = new ymaps.Map('map', {
+    var myMap = new ymaps.Map('map', {
         center: [55.7408157, 37.608925],
         zoom: 16,
         controls: []
     }, {
         searchControlProvider: 'yandex#search'
     });
+
+    var myGeoObject = new ymaps.GeoObject({
+        geometry: {
+            type: "Point",
+            coordinates: [55.7408157, 37.608925]
+        }}, {
+        iconLayout: 'default#image',
+        iconImageHref: 'static/images/pin.png',
+        iconImageSize: [30, 42],
+    });
+
+    myMap.geoObjects
+        .add(myGeoObject);
 
 }
