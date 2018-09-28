5a1a7f8db1a45c85054643639fa2a1324f475186
Mike Korneev <korneevm@gmail.com>
2018-09-28 11:56:28 +0300

    Fixed robots.txt

--- landing_page/mainpage/urls.py
+++ landing_page/mainpage/urls.py
@@ -1,10 +1,7 @@
 from django.urls import path
-from django.views.generic.simple import direct_to_template
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    (r'^robots\.txt$', direct_to_template,
-     {'template': 'robots.txt', 'mimetype': 'text/plain'})
 ]
