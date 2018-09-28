64add375b5f7cdf009704fdea98e2160ecc2b8c6
Simon Osipov <osipov.simon@gmail.com>
2018-09-28 10:38:11 +0200

    Added robot.txt

--- landing_page/mainpage/urls.py
+++ landing_page/mainpage/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from django.views.generic.simple import direct_to_template
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    (r'^robots\.txt$', direct_to_template,
+     {'template': 'robots.txt', 'mimetype': 'text/plain'})
 ]
