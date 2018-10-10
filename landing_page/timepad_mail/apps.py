687ec2ed715ab85ed44898a872685d15b76a78b4
Denis Trofimov <silaradost@yandex.ru>
2018-10-10 21:36:08 +0300

    Create 'python manage.py startapp timepad_mail' and add it to your INSTALLED_APPS in your settings file.

---
+++ landing_page/timepad_mail/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TimepadMailConfig(AppConfig):
+    name = 'timepad_mail'
---
+++ landing_page/timepad_mail/models.py
@@ -0,0 +1,4 @@
+"""Model to store message from timepad."""
+from django.db import models
+
+# Create your models here.
