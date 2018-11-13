8a542563b7c97e26b05fb462fa3364a1693c6a0f
Denis Trofimov <silaradost@yandex.ru>
2018-11-13 13:47:17 +0300

    Add parse_webhook_payload and STATUS_ACTION tio views.

--- landing_page/timepad_mail/views.py
+++ landing_page/timepad_mail/views.py
@@ -6,14 +6,51 @@ from django.conf import settings
 from django.http import HttpResponse, HttpResponseForbidden
 from django.views.decorators.csrf import csrf_exempt
 
-from .send_mail_mandrill import send_mail
+from .send_mail_mandrill import send_template, send_mail
 
+
+""" Status to action required correspondance.
+
+paid (оплачено): платный билет успешно оплачен он-лайн
+booked (забронировано): билет находится в статусе "Забронировано"
+notpaid (просрочено): билет не был оплачен и срок брони для него истек
+inactive (отказ): участник отказался от участия
+booked_offline (бронь для выкупа): билет был заказан для выкупа в офисе 
+организатора
+paid_offline (оплачено на месте): билет был оплачен в офисе организатора
+paid_ur (оплачено компанией): билет был оплачен юридическим платежом
+transfer_payment (перенесена оплата): билет был оплачен переносом оплаты 
+с другого заказа
+"""
+STATUS_ACTION = {
+    'paid': 'ticket-success',
+    'paid_ur': 'ticket-success',
+    'paid_offline': 'ticket-success',
+    'transfer_payment': 'ticket-success',
+    'inactive': 'ticket-cancel',
+    'notpaid': 'ticket-cancel',
+    'booked': 'ticket-creation',
+    'booked_offline': 'ticket-creation',
+}
 
 def process_webhook(payload):
     """Simple webhook handler that prints the event and payload."""
     print(json.dumps(payload, indent=4))
     result = send_mail(json.loads(payload))
     print(result)
+
+def parse_webhook_payload(payload: str):
+    try:
+        payload_dict = json.loads(payload)
+    except json.JSONDecodeError:
+        # TODO: log error
+        return
+    status = payload_dict.get('status_raw')
+    action = STATUS_ACTION.get(status)
+    if not action:
+        # no action required
+        return
+    # TODO: queue an action
 
 
 @csrf_exempt
