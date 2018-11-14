8949c30f06f85ca42501186b0c48fa436133b2b4
Denis Trofimov <silaradost@yandex.ru>
2018-11-14 16:11:35 +0300

    Add test_dict_deserialize.

--- landing_page/timepad_mail/tests.py
+++ landing_page/timepad_mail/tests.py
@@ -1,68 +1,72 @@
 import json
-from django.test import SimpleTestCase
+from django.test import SimpleTestCase, TestCase
+from django.utils import timezone
 
 from .send_mail_mandrill import (
     send_mail, _create_html_table_from_dict, send_template)
+from .models import Ticket, TicketQuerySet
 
+EMAIL = "denistrofimov@pythonmachinelearningcv.com"
+SURNAME = "Трофимов"
+NAME = "Денис"
+TIMEPAD_PAYLOAD = """
+{
+    "id": "22398586:56559903",
+    "event_id": 830329,
+    "organization_id": 143309,
+    "order_id": "17862035",
+    "reg_date": "2018-10-11 00:45:53",
+    "reg_id": 1993954,
+    "status": "\u0431\u0435\u0441\u043f\u043b\u0430\u0442\u043d\u043e",
+    "status_raw": "booked",
+    "email": "denistrofimov@pythonmachinelearningcv.com",
+    "surname": "Трофимов",
+    "name": "Денис",
+    "attended": false,
+    "code": "56559903",
+    "barcode": "1000565599031",
+    "price_nominal": "0",
+    "answers": [
+        {
+            "id": 3518729,
+            "type": "text",
+            "name": "E-mail",
+            "mandatory": null,
+            "value": "denistrofimov@pythonmachinelearningcv.com"
+        },
+        {
+            "id": 3518730,
+            "type": "text",
+            "name": "\u0424\u0430\u043c\u0438\u043b\u0438\u044f",
+            "mandatory": null,
+            "value": "denistrofimov"
+        },
+        {
+            "id": 3518731,
+            "type": "text",
+            "name": "\u0418\u043c\u044f",
+            "mandatory": null,
+            "value": "denistrofimov"
+        }
+    ],
+    "aux": {
+        "use_ticket_remind": "1"
+    },
+    "org_name": "PythonMachineLearningCV",
+    "event_name": "\u041d\u0430\u0437\u0432\u0430\u043d\u0438\u0435",
+    "event_city": "\u0411\u0435\u0437 \u0433\u043e\u0440\u043e\u0434\u0430",
+    "event_place": "",
+    "hook_generated_at": "2018-10-11 00:45:54",
+    "hook_guid": "710f9dbf-419b-409d-8b33-c265e6b0173d",
+    "hook_resend": 2
+}
+"""
 
 class MandrillSendTest(SimpleTestCase):
     """ Tests for send a transactional messages through Mandrill."""
     test_email = "denistrofimov@pythonmachinelearningcv.com"
     test_surname = "Трофимов"
     test_name = "Денис"
-
-    timepad_json_payload = """
-    {
-        "id": "22398586:56559903",
-        "event_id": 830329,
-        "organization_id": 143309,
-        "order_id": "17862035",
-        "reg_date": "2018-10-11 00:45:53",
-        "reg_id": 1993954,
-        "status": "\u0431\u0435\u0441\u043f\u043b\u0430\u0442\u043d\u043e",
-        "status_raw": "ok",
-        "email": "denistrofimov@pythonmachinelearningcv.com",
-        "surname": "trofimov",
-        "name": "denis",
-        "attended": false,
-        "code": "56559903",
-        "barcode": "1000565599031",
-        "price_nominal": "0",
-        "answers": [
-            {
-                "id": 3518729,
-                "type": "text",
-                "name": "E-mail",
-                "mandatory": null,
-                "value": "denistrofimov@pythonmachinelearningcv.com"
-            },
-            {
-                "id": 3518730,
-                "type": "text",
-                "name": "\u0424\u0430\u043c\u0438\u043b\u0438\u044f",
-                "mandatory": null,
-                "value": "denistrofimov"
-            },
-            {
-                "id": 3518731,
-                "type": "text",
-                "name": "\u0418\u043c\u044f",
-                "mandatory": null,
-                "value": "denistrofimov"
-            }
-        ],
-        "aux": {
-            "use_ticket_remind": "1"
-        },
-        "org_name": "PythonMachineLearningCV",
-        "event_name": "\u041d\u0430\u0437\u0432\u0430\u043d\u0438\u0435",
-        "event_city": "\u0411\u0435\u0437 \u0433\u043e\u0440\u043e\u0434\u0430",
-        "event_place": "",
-        "hook_generated_at": "2018-10-11 00:45:54",
-        "hook_guid": "710f9dbf-419b-409d-8b33-c265e6b0173d",
-        "hook_resend": 2
-    }
-    """
 
     def test_create_html_table_from_dict(self):
         """ Create HTML with sutable data from payload."""
@@ -77,7 +81,7 @@ class MandrillSendTest(SimpleTestCase):
         """ Test for send a new transactional message through Mandrill using 
             real timepad webhook json data.
         """
-        payload = json.loads(self.timepad_json_payload)
+        payload = json.loads(TIMEPAD_PAYLOAD)
         result = send_mail(payload)
         print(result)
         self.assertEqual(result[0]['status'], 'sent')
@@ -114,7 +118,7 @@ class MandrillSendTest(SimpleTestCase):
         )
         self.assertEqual(result[0]['status'], 'sent')
         self.assertEqual(result[0]['reject_reason'], None)
-        self.assertEqual(result[0]['email'], self.test_email)\
+        self.assertEqual(result[0]['email'], self.test_email)
 
     def test_send_template_ticket_creation(self):
         " Test for send a new message through Mandrill using a template."
@@ -162,4 +166,21 @@ class MandrillSendTest(SimpleTestCase):
             self.assertEqual(result[0]['status'], 'sent')
             self.assertEqual(result[0]['reject_reason'], None)
             self.assertEqual(result[0]['email'], self.test_email)
-            
+
+
+class TicketQuerySetTest(TestCase):
+
+    def test_dict_deserialize(self):
+        data = json.loads(TIMEPAD_PAYLOAD)
+        ticket = Ticket.objects.dict_deserialize(data)
+        self.assertEqual(ticket.order_id, 17862035)
+        self.assertEqual(ticket.event_id, 830329)
+        self.assertEqual(ticket.status, Ticket.STATUS_NEW)
+        self.assertEqual(
+            ticket.reg_date, 
+            timezone.datetime.strptime("2018-10-11 00:45:53", '%Y-%m-%d %H:%M:%S')
+        )
+        self.assertEqual(ticket.email, EMAIL)
+        self.assertEqual(ticket.name, NAME)
+        self.assertEqual(ticket.surname, SURNAME)
+        self.assertEqual(ticket.printed_id, "22398586:56559903")
