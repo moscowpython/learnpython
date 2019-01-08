3829d3769ac40a99f679e238a09008b3005f9429
Simon Osipov <osipov.simon@gmail.com>
2019-01-08 22:41:11 +0300

    Corrected URL for webhook

--- landing_page/mainpage/urls.py
+++ landing_page/mainpage/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('online', views.online, name='online'),
-    path('/api/webhook/', views.webhook, name='webhook')
+    path(r'^api/webhook/$', views.webhook, name='webhook')
 ]
