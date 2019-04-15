1c8fdde89f91d05e39cc4c3eb11f7330099d8838
Simon Osipov <osipov.simon@gmail.com>
2019-04-15 20:42:44 +0300

    Corrected urls

--- landing_page/mainpage/urls.py
+++ landing_page/mainpage/urls.py
@@ -4,6 +4,4 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('online', views.online, name='online'),
-    path('webhook', views.webhook, name='webhook')
 ]
