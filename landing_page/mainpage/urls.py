9aa3a5a2f7276e0bfdf871815ec6e7afa570cdc6
Simon Osipov <osipov.simon@gmail.com>
2019-04-21 13:27:28 +0300

    Removed slash

--- landing_page/mainpage/urls.py
+++ landing_page/mainpage/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('/projects', views.projects, name='projects')
+    path('projects', views.projects, name='projects')
 ]
