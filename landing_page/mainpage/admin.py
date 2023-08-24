bba99b19c98e93270dcd303a5cffa64787695d7d
Ilya Lebedev <melevir@gmail.com>
2023-08-24 16:52:13 +0200

    add enrollment to admin

--- landing_page/mainpage/admin.py
+++ landing_page/mainpage/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Curators, GraduateProjects
+from .models import Curators, Enrollment, GraduateProjects
 
 admin.site.register(Curators)
 admin.site.register(GraduateProjects)
+admin.site.register(Enrollment)
