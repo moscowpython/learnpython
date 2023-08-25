b7561fbfd5623a060efdb910092916a8dbfdcf51
Ilya Lebedev <melevir@gmail.com>
2023-08-25 16:17:07 +0200

    move enrollment to db

--- landing_page/tests/test_index.py
+++ landing_page/tests/test_index.py
@@ -1,8 +1,9 @@
 import pytest
 from django.test import Client
+from mainpage.models import Enrollment
 
 
 @pytest.mark.django_db
-def test__index__shows_up(client: Client) -> None:
+def test__index__shows_up(client: Client, active_enrollment: Enrollment) -> None:
     response = client.get('/')
     assert response.status_code == 200
